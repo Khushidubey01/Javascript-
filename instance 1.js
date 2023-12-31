c5a542df3fcdf2b09bfa73239affb4b901305a20
Archana Dubey <151169228+Khushidubey01@users.noreply.github.com>
2023-12-31 22:15:11 +0530

    Add files via upload

---
+++ instance 1.js
@@ -0,0 +1,13 @@
+<html>
+<body bgcolor="lightblue"text="deeppink"style="bold">
+<script type="text/javascript">
+var obj=new Object();
+obj.name="Teddy"
+obj.type="Thriller";
+obj.actor="Arya"
+obj.time_duration="2h.16min";
+document.write("Film Name="+obj.name+"<br>Type="+obj.type+"<br>Actor="+obj.actor+"<br>Time duration="+obj.time_duration);
+
+</script>
+</body>
+</html>
