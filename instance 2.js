839cf069eb80583ff3e8c3cf6bcd136da1c67436
Archana Dubey <151169228+Khushidubey01@users.noreply.github.com>
2023-12-31 22:17:58 +0530

    Add files via upload

---
+++ instance 2.js
@@ -0,0 +1,13 @@
+<html>
+<body bgcolor="lightblue"text="deeppink"style="bold">
+<script type="text/javascript">
+var obj=new Object();
+obj.name="Sohan"
+obj.Idno=102;
+obj.post="manager"
+obj.salery=24000;
+document.write("Employee name="+obj.name+"<br>Id number="+obj.Idno+"<br>Post="+obj.post+"<br>Salery="+obj.salery);
+
+</script>
+</body>
+</html>
---
+++ instance 3.js
@@ -0,0 +1,14 @@
+<html>
+<body bgcolor="lime">
+<script type="text/javascript">
+var obj=new Object();
+obj.name="Archana dubey"
+obj.rollno=2;
+obj.fname="Girijesh Dubey"
+obj.course="BCA"
+obj.fee=24000;
+document.write("name="+obj.name+"<br>Roll number="+obj.rollno+"<br>Father's name="+obj.fname+"<br>Course="+obj.course+"<br>Fee="+obj.fee);
+
+</script>
+</body>
+</html>
