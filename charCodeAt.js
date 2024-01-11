45955716c067b2a1598ca29e004b25cc29a8e4eb
Archana Dubey <151169228+Khushidubey01@users.noreply.github.com>
2024-01-11 20:04:47 +0530

    Add files via upload

---
+++ charCodeAt.js
@@ -0,0 +1,13 @@
+<html>
+<body>
+<script type="text/javascript">
+var  abc="ARCHANA";
+document.write("Here your string:"+abc);
+var i;
+for(i=0;i<abc.length;i++)
+{
+var a=abc.charCodeAt(i);
+document.write("<br>ASCII code:"+a); }
+</script>
+</body>
+</html>
