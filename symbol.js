c82ac8dec3fdf1313f643466d8b2d30d1e3a1c33
Archana Dubey <151169228+Khushidubey01@users.noreply.github.com>
2024-01-12 20:00:17 +0530

    Add files via upload

---
+++ symbol.js
@@ -0,0 +1,10 @@
+<html>
+<body bgcolor="silver"text="navy">
+<script type="text/javascript">
+
+for(var i=32;i<=255;i++)
+{document.write("<br>"+String.fromCharCode(i)+"="+i);
+}
+</script>
+</body>
+</html>
