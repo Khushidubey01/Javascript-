2d3f39df753c7bec87417f64f63b05f0b6eaa8ca
Archana Dubey <151169228+Khushidubey01@users.noreply.github.com>
2024-01-12 20:12:33 +0530

    Add files via upload

---
+++ indexOf.js
@@ -0,0 +1,19 @@
+<html>
+<body>
+<script type="text/javascript">
+var str="Archna";
+var a=str.indexOf('A');
+document.write("index of A="+a);
+var a=str.indexOf('r');
+document.write("<br>index of r="+a);
+var a=str.indexOf('c');
+document.write("<br>index of c="+a);
+var a=str.indexOf('h');
+document.write("<br>index of h="+a);
+var a=str.indexOf('n');
+document.write("<br>index of n="+a);
+var a=str.indexOf('a');
+document.write("<br>index of a="+a);
+</script>
+</body>
+</html>
